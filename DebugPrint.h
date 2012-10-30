41c82492e94e411986bab6a52642b0e49b07e29e
Beaver6813 <sam@beaver6813.com>
2012-10-30 16:04:56 +0000

    Remove printTime from WiShield debug files (unnecessary)

--- DebugPrint.h
+++ DebugPrint.h
@@ -6,7 +6,5 @@ extern "C" {
 	void DebugPrint(char *msg);
 	void DebugPrintF(const char *msg);
 	void DebugPrintFO(const char *msg);
-	void PrintTime(void);
-    void debug_print_flash(const char *s);
 }
 
